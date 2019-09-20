0ee14accf9fc8060c1067f68ddc04ef94cb137da
German Bortoli <bortolig@dnb.com>
2019-09-20 08:32:59 -0300

    Removed default state on enthusiasm action.

--- src/redux/enthusiasm/enthusiasm.reducer.ts
+++ src/redux/enthusiasm/enthusiasm.reducer.ts
@@ -2,14 +2,7 @@ import { EnthusiasmAction } from './enthusiasm.types'
 import { StoreState } from '../../types'
 import { INCREMENT_ENTHUSIASM, DECREMENT_ENTHUSIASM } from './enthusiasm.const'
 
-const defaulState = {
-  enthusiasmLevel: 1,
-}
-
-export default function enthusiasm(
-  state: StoreState = defaulState,
-  action: EnthusiasmAction,
-): StoreState {
+export default function enthusiasm(state: StoreState, action: EnthusiasmAction): StoreState {
   switch (action.type) {
     case INCREMENT_ENTHUSIASM:
       return { ...state, enthusiasmLevel: state.enthusiasmLevel + 1 }
