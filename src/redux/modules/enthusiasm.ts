e8d9c8ab438ce22e0aafe95d73bd1257ab6cde13
German Bortoli <bortolig@dnb.com>
2019-09-21 21:27:47 -0300

    Removed unused action type on enthusiasm

--- src/redux/modules/enthusiasm.ts
+++ src/redux/modules/enthusiasm.ts
@@ -24,8 +24,6 @@ export interface EnthusiasmDispatchProps {
   onDecrement: () => any
 }
 
-export type EnthusiasmAction = typeof enthusiasmDecrement | typeof enthusiasmIncrement
-
 const initialState: EnthusiasmStateProps = {
   enthusiasmLevel: 1,
 }
