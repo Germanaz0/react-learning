ac6830bc6012bf6f11e55fb1eccc3a8db3c758b5
German Bortoli <bortolig@dnb.com>
2019-09-23 07:35:29 -0300

    Fixed app test

--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -4,6 +4,11 @@ import App from '../App/App'
 
 it('renders without crashing', () => {
   const div = document.createElement('div')
-  ReactDOM.render(<App />, div)
+  const onIncrement = { type: 'ENTHUSIASM_INCREMENT' }
+  const onDecrement = { type: 'ENTHUSIASM_INCREMENT' }
+  ReactDOM.render(
+    <App enthusiasmLevel={1} onDecrement={() => onDecrement} onIncrement={() => onIncrement} />,
+    div,
+  )
   ReactDOM.unmountComponentAtNode(div)
 })
