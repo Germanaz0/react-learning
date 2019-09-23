25d12b9779e730996b917683562183d04b9648bd
German Bortoli <bortolig@dnb.com>
2019-09-23 15:56:29 -0300

    Main refactor of storage props

--- src/utils/storage/index.ts
+++ src/utils/storage/index.ts
@@ -1,2 +1,25 @@
-export * from './AppStorage'
+import { AppStorage } from './AppStorage'
+import { AppStorageTypes } from './storage.types'
+
 export * from './storage.types'
+
+/**
+ * Get an instance of app storage ready to use with local storage.
+ */
+export const getLocalStorage = () => {
+  return new AppStorage(AppStorageTypes.LOCAL)
+}
+
+/**
+ * Get an instance of app storage ready to use with session storage
+ */
+export const getSessionStorage = () => {
+  return new AppStorage(AppStorageTypes.SESSION)
+}
+
+/**
+ * Get an instance of app storage ready to use with cookies.
+ */
+export const getCookieStorage = () => {
+  return new AppStorage(AppStorageTypes.COOKIE)
+}
